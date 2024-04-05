064a17bd7c235641fb70df58d14a5842252c29f7
aliveevie <aliveibi080@gmail.com>
2024-04-05 13:44:07 +0100

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 4500;
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is Listening on port ${port}`)
+})
